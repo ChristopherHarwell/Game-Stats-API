26d047a8fa3ee374d17278823ba05ae0adb95437
Christopher Harwell <chrisharwell72@gmail.com>
2022-06-26 23:22:53 -0400

    Adds GET req to trn api

--- src/match-history/match-history.controller.ts
+++ src/match-history/match-history.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
+import axios from 'axios';
 import { Request, Response } from 'express';
 @Controller('match-history')
 export class MatchHistoryController {
     @Get('match-history/:platform/:platformUserId')
     findHistoryBySession(@Req() request: Request, @Res() response: Response): any {
+        axios.get(`https://public-api.tracker.gg/v2/apex/standard/profile/${request.params.platform}/${request.params.platformUserId}/sessions`).then(res => {
+            response.send(res.data);
+        }).catch(err => {
+            response.send(err);
+        })
     }
 
 }
